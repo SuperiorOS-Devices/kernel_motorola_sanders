83dd76d07ebebebcf62cd195c5f22230d6771f1c
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-13 04:15:59 +0000

    msm: vidc: disable debug logs
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -17,8 +17,9 @@
 #include "msm_vidc_debug.h"
 #include "vidc_hfi_api.h"
 
-#define MAX_DBG_BUF_SIZE 4096
 int msm_vidc_debug = 0;
+EXPORT_SYMBOL(msm_vidc_debug);
+
 int msm_vidc_debug_out = VIDC_OUT_PRINTK;
 int msm_vidc_fw_debug = 0x18;
 int msm_vidc_fw_debug_mode = 1;
